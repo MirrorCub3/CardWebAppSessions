2bd95497fe26f8ad262659ad6b4224e0fc40f804
wcf <54386460+trappedintokyo@users.noreply.github.com>
2021-06-10 13:13:34 -0700

    Add files via upload

--- public/js/join.js
+++ public/js/join.js
@@ -13,10 +13,20 @@ $( document ).ready(function() {
             
                 $("#messages").append('<li>' + data.host+ data.name+ data.players+ data.jokers+ data.infinite +'</li>');
                 alert("good read");
+                var identYY = data.ident;
+                var r= $('<input type="button" value="Join Game" id = data.ident + "button"/>');
+                $("messages").append(r);
               }
             } ,     
             dataType: "json"
           });     
   		  return false;
   		}   
-});
+      $("#data.iddent+'button'").click(sendtoGame);
+    });
+function sendtoGame(){
+  $.post("/sendtogame",{},function(data)
+  {
+    window.location = data.redirect;
+  });}
+
