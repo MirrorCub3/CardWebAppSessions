ecd467bc2d1ab28a76d603b07f3671bbd7848d1a
MirrorCub3 <joycemai.exe@gmail.com>
2021-06-10 12:42:52 -0700

    Update routesGame.js

--- routesGame.js
+++ routesGame.js
@@ -62,8 +62,7 @@ function initGameIdent(){ // check everytime a ident is gotten to update it to t
 router.post("/creategame", function(req, res) {
   //console.log(req);
 console.log("post creategame");
-console.log("routes game ident before" + gameIdent);
-  initGameIdent(); // put a promise here
+  initGameIdent();
   gameIdent++; // plus one to ident = notice how the id is already ++ even though it hasn't passes the find null check yet - try moving this below the return line and see if it causes issues
 console.log("routes game ident after" + gameIdent);
   var newGame = new GameSettingsJS(
