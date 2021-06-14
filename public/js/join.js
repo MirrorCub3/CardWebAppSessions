459ab61a76964d77577a9b6b98683980b4a353c6
MirrorCube <joycemai.exe@gmail.com>
2021-06-13 20:15:23 -0700

    Update join.js

--- public/js/join.js
+++ public/js/join.js
@@ -19,7 +19,7 @@ function getGames() {
         if(data.info[i].gameActive == true){
         console.log(data.info[i].name);
         $("#allGames").append("<div class =" + data.info[i].ident + "  id = 'child'></div>");
-        $("."+data.info[i].ident+"").append("<section><h2 id = 'gameName'> "+data.info[i].name+"</h2><p id = 'playerNum'>Active Players: "+data.info[i].players+"</p>  <p id = 'hostName'> Host: "+data.info[i].host+"</p><input id='joinButton"+data.info[i].dent+"' type='button' value=' Join Game ' onclick = 'joinGame(this)'/></section>");
+        $("."+data.info[i].ident+"").append("<section><h2 id = 'gameName'> "+data.info[i].name+"</h2><p id = 'playerNum'>Active Players: "+data.info[i].players+"</p>  <p id = 'hostName'> Host: "+data.info[i].host+"</p><input id='joinButton"+data.info[i].ident+"' type='button' value=' Join Game ' onclick = 'joinGame(this)'/></section>");
         }
       }
     });
@@ -54,7 +54,7 @@ function logoutClicked(){ //logout function
 function joinGame(element){
 console.log("joinclicked");
 console.log(element);
-let str = toString(element.id);
+let str = element.id.toString();
 console.log(str);
 console.log(str.substring(10,str.length));
 }
